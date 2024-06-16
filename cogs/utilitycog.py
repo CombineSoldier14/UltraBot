152923051c3fa36d6df433bcd5a34ea2add5dbad
『CombineSoldier14』 <101304773+CombineSoldier14@users.noreply.github.com>
2024-06-16 14:46:47 -0700

    Update utilitycog.py

--- cogs/utilitycog.py
+++ cogs/utilitycog.py
@@ -7,29 +7,13 @@ import uuid
 import string
 import random
 import time
+import cogs.combinebot
+from cogs.combinebot import name
+from cogs.combinebot import game
+from cogs.combinebot import icon
+from cogs.combinebot import VERSION
+from cogs.combinebot import LATESTADDITION
 
-with open("version.json", "r") as f:
-            _r = json.load(f)
-            VERSION = _r["VERSION"]
-
-with open("dev.json", "r") as f:
-            _r = json.load(f)
-            dev_status = _r["DEV_STATUS"]
-
-
-
-#The Dev status is meant for if CombineBot is running in DEV mode which changes some names and icons.
-
-
-if dev_status == "true":
-            name = "CombineBot Development Edition"
-            game = "with unstable ass commands"
-            icon = "https://cdn.discordapp.com/app-icons/1227477531461025854/85f59950e14cca56e4b1bcefd911ca23.png?size=256"
-
-if dev_status == "false":
-            name = "CombineBot"
-            game = "combinesoldier14.site"
-            icon = "https://i.postimg.cc/j5YGqs0n/f66bd4beb4f1ebee0685d8c5cfd646bb.png"
 
 
 class Utilitycog(commands.Cog):
@@ -44,12 +28,11 @@ class Utilitycog(commands.Cog):
 
 
     @group.command(name="makeembed", description="Make your own embed!")
-    async def makeembed(self, ctx, title: discord.Option(str, description="Title of embed"), description: discord.Option(str, description="Description of embed"), footer: discord.Option(str, description="Footer of embed"), color: discord.Option(int, description="Color of embed in hex format")):
-        embed = discord.Embed(
-            title=title,
-            description=description,
-            color=color,
-        )
+    async def makeembed(self, ctx, title: discord.Option(str, description="Title of embed"), 
+                        description: discord.Option(str, description="Description of embed"), 
+                        footer: discord.Option(str, description="Footer of embed"), 
+                        color: discord.Option(int, description="Color of embed in hex format")):
+        embed = cogs.combinebot.makeEmbed(title=title, description=description, color=color)
         embed.set_footer(text=footer)
         await ctx.respond(embed=embed)
 
@@ -62,12 +45,10 @@ class Utilitycog(commands.Cog):
     @commands.has_permissions(manage_channels=True)
     async def timestop(self, ctx):
         await ctx.channel.set_permissions(ctx.guild.default_role, send_messages=False)
-        embed = discord.Embed(
-            title="ZA WARUDO!",
-            description="Time has been stopped! No messages can be sent except for admins.",
-            color=discord.Colour.red(),
-        )
-        embed.set_footer(text="{0} v{1}".format(name, VERSION), icon_url=icon)
+        embed = cogs.combinebot.makeEmbed(title="ZA WARUDO!", 
+                                          description="Time has been stopped! No messages can be sent except for admins.", 
+                                          color=discord.Colour.red(), 
+                                          footer="{0} v{1}".format(name, VERSION), icon_url=icon)
         embed.set_image(url="https://i.redd.it/05vtn9chak101.gif")
         await ctx.respond(embed=embed)
 
@@ -80,52 +61,40 @@ class Utilitycog(commands.Cog):
     @group.command(name="userinfo", description="Gets info on a user in the server!")
     async def userinfo(self, ctx, user: discord.Option(discord.Member, description="User to get info of", required=True)):
         if user.bot == True:
-         embed = discord.Embed(
-             title="Info on {0}".format(user),
-             description="""
+         embed = cogs.combinebot.makeEmbed(title="Info on {0}".format(user), 
+                                           description="""
              **ID:** {0}
-             **Joined Discord:** {1}
-             **Discriminator:** {2} 
-             **Is A Bot?:** {3} 
-             """.format(user.id, user.created_at, user.discriminator, user.bot),
-             color=user.color,
-          
-         )
-         embed.set_footer(text="{0} v{1}".format(name, VERSION), icon_url=icon)
+**Joined Discord:** {1}
+**Discriminator:** {2} 
+**Is A Bot?:** Yes
+             """.format(user.id, user.created_at, user.discriminator),
+             color=user.color)
          embed.set_thumbnail(url=user.avatar)
         
         if user.bot == False:
     
-         embed = discord.Embed(
-             title="Info on {0}".format(user),
-             description="""
+         embed = cogs.combinebot.makeEmbed(title="Info on {0}".format(user), 
+                                           description="""
              **ID:** {0}
-             **Joined Discord:** {1} 
-             **Is A Bot?:** {2} 
-             """.format(user.id, user.created_at, user.bot),
-             color=user.color,
-          
-         )
-         embed.set_footer(text="{0} v{1}".format(name, VERSION), icon_url=icon)
+**Joined Discord:** {1} 
+**Is A Bot?:** No 
+             """.format(user.id, user.created_at, user.discriminator),
+             color=user.color)
          embed.set_thumbnail(url=user.avatar)
         await ctx.respond(embed=embed)
           
 
     @group.command(name="botinfo", description="Info about UltraBot!")
     async def botinfo(self, ctx):
-         embed = discord.Embed(
-              title="Bot Info",
-              description="""
-              **Bot Name:** UltraBot
-              **Bot Owner:** @combinesoldier14
-              **Creation Date:** 4/5/2024
-              **Server Count:** Unavailable
-              **Library**: Py-cord {0}
-              """.format(discord.__version__),
-              color=discord.Colour.og_blurple(),
-         )
-         embed.set_footer(text="{0} v{1}".format(name, VERSION), icon_url=icon)
-         embed.set_thumbnail(url="https://cdn.discordapp.com/app-icons/1225220764861730867/f66bd4beb4f1ebee0685d8c5cfd646bb.png?size=256")
+         embed = cogs.combinebot.makeEmbed(title="Bot Info", 
+                                           description="""
+**Bot Name:** {0}
+**Bot Owner:** @combinesoldier14
+**Creation Date:** 4/5/2024
+**Library**: Py-cord v{1}
+              """.format(name, discord.__version__),
+              color=discord.Colour.og_blurple())
+         embed.set_thumbnail(url=icon)
          await ctx.respond(embed=embed)
 
     @group.command(name="channelinfo", description="Shows detailed info on a server channel.")
