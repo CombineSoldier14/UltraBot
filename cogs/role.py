45535ec79002e3af294235ae83484b947f05953c
『CombineSoldier14』 <101304773+CombineSoldier14@users.noreply.github.com>
2024-06-16 19:48:03 -0700

    Update role.py

--- cogs/role.py
+++ cogs/role.py
@@ -4,29 +4,13 @@ import os
 from discord import reaction
 from discord import Reaction
 import json
+import cogs.combinebot
+from cogs.combinebot import name
+from cogs.combinebot import game
+from cogs.combinebot import icon
+from cogs.combinebot import VERSION
+from cogs.combinebot import LATESTADDITION
 
-
-with open("version.json", "r") as f:
-            _r = json.load(f)
-            VERSION = _r["VERSION"]
-
-with open("dev.json", "r") as f:
-            _r = json.load(f)
-            dev_status = _r["DEV_STATUS"]
-
-
-#The Dev status is meant for if CombineBot is running in DEV mode which changes some names and icons.
-
-
-if dev_status == "true":
-            name = "CombineBot Development Edition"
-            game = "with unstable ass commands"
-            icon = "https://cdn.discordapp.com/app-icons/1227477531461025854/85f59950e14cca56e4b1bcefd911ca23.png?size=256"
-
-if dev_status == "false":
-            name = "CombineBot"
-            game = "combinesoldier14.site"
-            icon = "https://i.postimg.cc/j5YGqs0n/f66bd4beb4f1ebee0685d8c5cfd646bb.png"
 
 
 class Role(commands.Cog):
@@ -60,7 +44,7 @@ class Role(commands.Cog):
 
     @group.command(name="roleinfo", description="Gets detailed info on a role")
     async def roleinfo(self, ctx, role: discord.Option(discord.Role, description="Role to get info on")):
-         embed = discord.Embed(
+         embed = cogs.combinebot.makeEmbed(
               title = "Info on {0}".format(role.name),
               description = """
               **Created at:** {0} 
@@ -73,7 +57,6 @@ class Role(commands.Cog):
               color = role.colour,
          
          )
-         embed.set_footer(text="{0} v{1}".format(name, VERSION), icon_url=icon)
          
          await ctx.respond(embed=embed)
 
@@ -81,3 +64,4 @@ class Role(commands.Cog):
 
 def setup(bot): # this is called by Pycord to setup the cog
     bot.add_cog(Role(bot)) # add the cog to the bot
+
