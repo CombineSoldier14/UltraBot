ab15498fecb8cb56cffb7988715f3194534fb008
『CombineSoldier14』 <101304773+CombineSoldier14@users.noreply.github.com>
2024-06-16 19:36:38 -0700

    Update fun.py

--- cogs/fun.py
+++ cogs/fun.py
@@ -9,31 +9,19 @@ import cowsay
 import nltk
 import random
 import json
+import cogs.combinebot
+from cogs.combinebot import name
+from cogs.combinebot import game
+from cogs.combinebot import icon
+from cogs.combinebot import VERSION
+from cogs.combinebot import LATESTADDITION
+
+
 nltk.download('words')
 
 word_list = ["Heads", "Tails"]
 eightball_list = ["It is certain.", "It is decidedly so", "Without a doubt", "Yes - definitely", "You may rely on it", "As I see it, yes.", "Most likely.", "Outlook good.", "Yes.", "Signs point to yes.", "Reply hazy, try again", "Ask again later", "Better not tell you now.", "Cannot predict now.", "Concentrate and ask again.", "Don't count on it", "My reply is no.", "My sources say no.", "Outlook not so good.", "Very doubtful."]
-with open("version.json", "r") as f:
-            _r = json.load(f)
-            VERSION = _r["VERSION"]
 
-with open("dev.json", "r") as f:
-            _r = json.load(f)
-            dev_status = _r["DEV_STATUS"]
-
-
-#The Dev status is meant for if CombineBot is running in DEV mode which changes some names and icons.
-
-
-if dev_status == "true":
-            name = "CombineBot Development Edition"
-            game = "with unstable ass commands"
-            icon = "https://cdn.discordapp.com/app-icons/1227477531461025854/85f59950e14cca56e4b1bcefd911ca23.png?size=256"
-
-if dev_status == "false":
-            name = "CombineBot"
-            game = "combinesoldier14.site"
-            icon = "https://i.postimg.cc/j5YGqs0n/f66bd4beb4f1ebee0685d8c5cfd646bb.png"
 
 
 class Fun(commands.Cog):
@@ -162,19 +150,13 @@ class Fun(commands.Cog):
 
     @group.command(name="randomyt", description="Get a random youtube video")
     async def randomyt(self, ctx):
-         
-         listvalues = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
-         videoid = ""
-         for _ in range(11): # video id is 11 chars long
-            videoid = videoid + random.choice(listvalues)
-         
-         await ctx.respond("https://www.youtube.com/watch?v={}".format(videoid))
+         await ctx.respond(cogs.combinebot.getYTvideo())
          await ctx.send("NOTE: Not all generated videos may exist.")
 
     @group.command(name="aaquote", description="Get a random Ace Attorney quote!")
     async def aaquote(self, ctx):
           await ctx.respond(
-                random.choice(self.quotes)
+                cogs.combinebot.getAceAttorneyQuote()
           )
 
 def setup(bot): # this is called by Pycord to setup the cog
