27490c4e0e8dc32bf3a1c9f6d33a48719fc2779b
『CombineSoldier14』 <101304773+CombineSoldier14@users.noreply.github.com>
2024-06-23 19:03:04 -0700

    Update lists.py

--- cogs/lists.py
+++ cogs/lists.py
@@ -248,3 +248,33 @@ mbtifuncs = [
     "stack":"Ne-Fi-Te-Si"
 },
 ]
+
+difficulty = ["easy", "medium", "hard"]
+category = [
+    "Any Category",
+    "General Knowledge",
+    "Entertainment: Books",
+    "Entertainment: Film",
+    "Entertainment: Music",
+    "Entertainment: Musicals & Theatres",
+    "Entertainment: Television",
+    "Entertainment: Video Games",
+    "Entertainment: Board Games",
+    "Science & Nature",
+    "Science: Computers",
+    "Science: Mathematics",
+    "Mythology",
+    "Sports",
+    "Geography",
+    "History",
+    "Politics",
+    "Art",
+    "Celebrities",
+    "Animals",
+    "Vehicles",
+    "Entertainment: Comics",
+    "Science: Gadgets",
+    "Entertainment: Japanese Anime & Manga",
+    "Entertainment: Cartoon & Animations"
+
+]
